a797a76bd7dfaf674029f53bffe92f4cf682d66a
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-02-13 14:55:44 +0200

    fix some linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -86,14 +86,14 @@ module Enumerable
   end
 
   def my_count(obj = nil)
-    count = 0
+    count = self.length
     # if object is given
     unless obj.nil?
       find = obj
       (0..length - 1).each do |i|
         count += 1 if self[i] == find
       end
-    return length if obj.nil?
+      return length if obj.nil?
     end
 
     (0..length - 1).each do |i|
@@ -162,62 +162,64 @@ def multiply_els(obj)
   obj.my_inject(1, :*)
 end
 
-# my_each_with_index
-# hash = Hash.new
-# %w(cat dog wombat).my_each_with_index { |item, index|
-#   hash[item] = index
-# }
-# print hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
 
-# my_select
-# (1..10).find_all { |i|  i % 3 == 0 }   #=> [3, 6, 9]
+# Tests
+  # my_each_with_index
+    # hash = Hash.new
+    # %w(cat dog wombat).my_each_with_index { |item, index|
+    #   hash[item] = index
+    # }
+    # print hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
 
-# print [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
+  # my_select
+    # print (1..10).my_select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
 
-# my_all?
-# puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-# puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-# puts %w[ant bear cat].my_all?(/t/)                        #=> false
-# puts [1, 2i, 3.14].my_all?(Numeric)                       #=> true
-# puts [nil, true, 99].my_all?                              #=> false
-# puts [].my_all?                                           #=> true
+    # print [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
 
-# my_any?
-# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
-# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
-# puts %w[ant bear cat].my_any?(/d/)                        #=> false
-# puts [nil, true, 99].my_any?(Integer)                     #=> true
-# puts [nil, true, 99].my_any?                              #=> true
-# puts [].any?                                              #=> false
+  # my_all?
+    # puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+    # puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+    # puts %w[ant bear cat].my_all?(/t/)                        #=> false
+    # puts [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+    # puts [nil, true, 99].my_all?                              #=> false
+    # puts [].my_all?                                           #=> true
 
-# my_none?
-# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-# puts %w{ant bear cat}.my_none?(/d/)                        #=> true
-# puts [1, 3.14, 42].my_none?(Float)                         #=> false
-# puts [].my_none?                                           #=> true
-# puts [nil].my_none?                                        #=> true
-# puts [nil, false].my_none?                                 #=> true
-# puts [nil, false, true].my_none?                          #=> false
+  # my_any?
+    # puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+    # puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+    # puts %w[ant bear cat].my_any?(/d/)                        #=> false
+    # puts [nil, true, 99].my_any?(Integer)                     #=> true
+    # puts [nil, true, 99].my_any?                              #=> true
+    # puts [].any?                                              #=> false
 
-# my_count
-#  ary = [1, 2, 4, 2]
-# puts ary.my_count               #=> 4
-# puts ary.my_count(2)            #=> 2
-# puts ary.my_count{ |x| x%2==0 } #=> 3
+  # my_none?
+    # puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+    # puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+    # puts %w{ant bear cat}.my_none?(/d/)                        #=> true
+    # puts [1, 3.14, 42].my_none?(Float)                         #=> false
+    # puts [].my_none?                                           #=> true
+    # puts [nil].my_none?                                        #=> true
+    # puts [nil, false].my_none?                                 #=> true
+    # puts [nil, false, true].my_none?                          #=> false
 
-# my_map
-# print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
-# puts ""
-# print (1..4).my_map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+  # my_count
+    # ary = [1, 2, 4, 2]
+    # puts ary.my_count               #=> 4
+    # puts ary.my_count(2)            #=> 2
+    # puts ary.my_count{ |x| x%2==0 } #=> 3
 
-# my_inject
-# puts (5..10).my_inject { |sum, n| sum + n }            #=> 45
-# puts (5..10).my_inject(:+)                             #=> 45
-# puts (5..10).my_inject(1, :*)                          #=> 151200
-# puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
-# longest = %w{ cat sheep bear }.my_inject {|memo, word|
-#   memo.length > word.length ? memo : word}
-# puts longest                                        #=> "sheep"
-# multiply_els
-# puts multiply_els([2,4,5]) #=> 40
+  # my_map
+    # print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
+    # puts ""
+    # print (1..4).my_map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+
+  # my_inject
+    # puts (5..10).my_inject { |sum, n| sum + n }            #=> 45
+    # puts (5..10).my_inject(:+)                             #=> 45
+    # puts (5..10).my_inject(1, :*)                          #=> 151200
+    # puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+    # longest = %w{ cat sheep bear }.my_inject {|memo, word|
+    #   memo.length > word.length ? memo : word}
+    # puts longest                                        #=> "sheep"
+  # multiply_els
+    # puts multiply_els([2,4,5]) #=> 40
