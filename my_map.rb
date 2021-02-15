f87968d73c0490151494cd4aeb086e9993556336
erez <erezfree29@gmail.com>
2021-02-15 06:23:10 -0800

    method to work with both cases

--- my_map.rb
+++ my_map.rb
@@ -1,15 +1,19 @@
 module Enumerable
-    def my_map(block)
+    def my_map(block = nil)
         new_array = []
         array = to_a
         (0..array.length - 1).each do |i|
-        item = block.call(array[i])
-        new_array << item
+            if block != nil    
+                item = block.call(array[i])
+            else
+                item = yield(array[i],i)    
+            end
+            new_array << item
         end
         new_array
     end
 
-
     print (1..4).my_map{|x| x*2} 
+    proc1 = Proc.new {|x| x**2 }
     print (1..4).my_map(proc1) 
 end
